ea692fc255f67e41fc7270f4f4ad7e088f244f74
mixamum <mixamum@users.noreply.github.com>
2021-08-03 16:45:46 -0400

    Basket Complete

--- src/Home.js
+++ src/Home.js
@@ -16,14 +16,14 @@ function Home() {
           <Product
             id="4903850"
             title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses [By ER]-[Paperback]"
-            price="29.99"
+            price={29.99}
             image="https://images-na.ssl-images-amazon.com/images/I/41Ag4WE7uyL._SX324_BO1,204,203,200_.jpg"
             rating={5}
           />
           <Product
             id="23445930"
             title="HAUSWIRT 3-IN-1 Stand Mixer, 5.3Qt 550W 8-Speed Tilt-Head Electric Kitchen Mixers with Digital Display, Power Hub for Attachments, Food Mixer with Dough Hook, Wire Whip, Mixing Beater- Baby Blue"
-            price="199.99"
+            price={199.99}
             image="https://m.media-amazon.com/images/I/71dR+4gxPAL._AC_SL1500_.jpg"
             rating={4}
           />
@@ -33,21 +33,21 @@ function Home() {
           <Product
             id="48017462"
             title="SAMSUNG Galaxy S21+ Plus 5G Factory Unlocked Android Cell Phone 128GB US Version Smartphone Pro-Grade Camera 8K Video 12MP High Res, Phantom Black"
-            price="999.99"
+            price={999.99}
             image="https://m.media-amazon.com/images/I/61cjeSE+Z-L._AC_SL1000_.jpg"
             rating={5}
           />
           <Product
             id="340145701"
             title='AOC C24G1A 24" Curved Frameless Gaming Monitor, FHD 1920x1080, 1500R, VA, 1ms MPRT, 165Hz (144Hz supported), FreeSync Premium, Height adjustable Black'
-            price="192.98"
+            price={192.98}
             image="https://m.media-amazon.com/images/I/81GJkTKuLnL._AC_SL1500_.jpg"
             rating={5}
           />
           <Product
             id="480174014"
             title="Echo Dot (4th Gen) | Smart speaker with Alexa | Twilight Blue"
-            price="49.99"
+            price={49.99}
             image="https://m.media-amazon.com/images/I/71UCjudB1cL._AC_SL1000_.jpg"
             rating={5}
           />
@@ -57,7 +57,7 @@ function Home() {
           <Product
             id="95185829"
             title="New Apple Watch Series 6 (GPS, 40mm) - Blue Aluminum Case with Deep Navy Sport Band"
-            price="349.99"
+            price={349.99}
             image="https://m.media-amazon.com/images/I/71bf9IpGjtL._AC_SL1500_.jpg"
             rating={5}
           />
