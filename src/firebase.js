10050de8623f2a035c996f70ff8f6b3fde56f53f
mixamum <mixamum@users.noreply.github.com>
2021-08-04 15:54:55 -0400

    Login and Register Done!

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDwzEI48zhV2qyQbDPs9bjsHLu7mKR9Odw",
-    authDomain: "clone-514ee.firebaseapp.com",
-    projectId: "clone-514ee",
-    storageBucket: "clone-514ee.appspot.com",
-    messagingSenderId: "1074412723003",
-    appId: "1:1074412723003:web:e0b2ee308dcfaffaccb51e",
-    measurementId: "G-Q6YWMSN50C"
-  };
+  apiKey: "AIzaSyDwzEI48zhV2qyQbDPs9bjsHLu7mKR9Odw",
+  authDomain: "clone-514ee.firebaseapp.com",
+  projectId: "clone-514ee",
+  storageBucket: "clone-514ee.appspot.com",
+  messagingSenderId: "1074412723003",
+  appId: "1:1074412723003:web:e0b2ee308dcfaffaccb51e",
+  measurementId: "G-Q6YWMSN50C",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
